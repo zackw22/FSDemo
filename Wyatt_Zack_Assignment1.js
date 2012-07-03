7f8c4e35f84a5a6c11d707b27ee5587f16c8f15d
Zack Wyatt <Zack@Zacks-MacBook-Pro.local>
2012-07-02 21:01:59 -0600

    Added first files to the repo

---
+++ Wyatt_Zack_Assignment1.js
@@ -0,0 +1,36 @@
+//Zack Wyatt
+//Project 1
+//June 28th 2012
+//Game Winner?
+
+var player = "Jebron Lames";
+var	team1 = "The Flame";
+var	team2= "The Lightening";
+var	secondsleft= "5";
+var score= "97 to 99";
+var myNum= true;
+var win= "The crowd goes wild!";
+var loses= "The crowd boos him off the court!";
+
+//Variable output
+
+console.log 	
+ {
+	console.log ("This is a game of "+ team1+ " vs "+ team2);
+	}
+{
+	console.log (player +" has the ball with " + secondsleft + " seconds on the clock");
+	}	
+{	
+	console.log ("There's " + secondsleft + " seconds left on the clock and " + team2+ " are ahead "+ score);
+	}
+	
+// The Conditional	
+	
+	if (shootsScore=true)
+{	console.log (player + " takes the shot and " + win); 
+	}else 
+	console.log (player + " takes the shot and " + loses);
+	
+	
+	
